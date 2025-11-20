00f405b293303243d46eb43bc0a2a207ca77438f
CornyShawny <160194015+CornyShawny@users.noreply.github.com>
2025-11-20 15:07:24 +1100

    work
    almost done

--- database/queries.sql
+++ database/queries.sql
@@ -2,21 +2,26 @@
 --                       firstname TEXT NOT NULL,
 --                       lastname TEXT NOT NULL,
 --                       dob TEXT NOT NULL);
+
 -- CREATE TABLE Marks(id INTEGER PRIMARY KEY AUTOINCREMENT,
 --                       student_id INTEGER,
 --                       subject TEXT NOT NULL,
 --                       mark INTEGER);
+
 -- INSERT INTO Students(firstname, lastname, dob)
 --               VALUES('Lachlan', 'Snake', '26/09/2007');
+
 -- INSERT INTO Students(firstname, lastname, dob) VALUES
 --             ('Bobby', 'Bob', '3/01/2008'),
 --             ('Bjorn', 'Johnson', '28/02/2007');
+
 -- INSERT INTO Students(firstname, lastname, dob) VALUES
 --             ('Tabitha', 'Michael', '23/10/2007'),
 --             ('Bart', 'Simpson', '25/11/2007'),
 --             ('Jennifer', 'Rose', '14/09/2007'),
 --             ('Lequisha', 'Dequavious', '11/10/2007'),
 --             ('Jill', 'Bloodborne', '28/03/2007');
+
 -- INSERT INTO Marks(student_id, subject, mark) VALUES
 --                 (1, 'English', 50),
 --                 (1, 'Maths', 100),
@@ -26,11 +31,76 @@
 --                 (4, 'English', 97),
 --                 (5, 'Maths', 82),
 --                 (5, 'Science', 5);
-SELECT * FROM Students;
 
-SELECT firstname, lastname FROM Students;
+-- SELECT * FROM Students;
 
-SELECT * FROM Students LIMIT 5;
+-- SELECT firstname, lastname FROM Students;
 
-SELECT firstname, dob from Students 
-    WHERE firstname LIKE 'B%';
+-- SELECT * FROM Students LIMIT 5;
+
+-- SELECT firstname, dob from Students 
+--     WHERE firstname LIKE 'B%';
+
+-- SELECT firstname, lastname FROM Students
+-- ORDER BY lastname ASC;
+
+-- SELECT * FROM Students
+-- WHERE dob LIKE '%2007%';
+
+-- SELECT * FROM Marks
+
+-- SELECT * FROM Marks
+-- WHERE subject LIKE '%English%';
+
+-- SELECT subject, mark FROM Marks
+-- GROUP BY id
+-- HAVING mark < 50;
+
+-- SELECT subject, mark FROM Marks
+-- GROUP BY id
+-- HAVING mark >= 50;
+
+-- DELETE FROM Students
+--     WHERE id = 2;
+
+-- INSERT INTO Students(firstname, lastname, dob)
+--              VALUES ('Bobby', 'Bob', '3/1/2008');
+
+-- DELETE FROM Marks
+--     WHERE mark < 25;
+
+-- DELETE FROM Students
+-- WHERE lastname LIKE '%Simpson%';
+
+-- DELETE FROM Marks
+-- WHERE mark > 100;
+
+-- SELECT COUNT(id), firstname
+--        FROM Students
+--        GROUP BY firstname;
+
+-- SELECT SUM(mark), subject
+--        FROM Marks
+--        GROUP BY subject;
+
+-- SELECT AVG(mark), subject
+--        FROM Marks
+--        GROUP BY subject;
+
+-- SELECT COUNT(lastname), lastname
+-- FROM Students
+-- GROUP BY lastname;
+
+-- SELECT MAX(mark), subject
+-- FROM Marks
+-- GROUP BY subject;
+
+-- SELECT COUNT(subject), subject
+-- FROM Marks
+-- GROUP BY subject;
+
+-- SELECT Students.firstname, Students.lastname,
+--         Marks.subject, Marks.mark
+-- FROM Students JOIN Marks
+-- ON Students.id=MArks.student_id;
+
